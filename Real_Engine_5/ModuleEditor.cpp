a570ad486dc13aad2be70ba99ca9b20b834edf55
Hacienda69 <fannumero1dehacienda@gmail.com>
2023-09-29 10:01:44 +0200

    da errores, acabo en casa

--- Real_Engine_5/ModuleEditor.cpp
+++ Real_Engine_5/ModuleEditor.cpp
@@ -1,5 +1,6 @@
 #include "ModuleEditor.h"
-
+//#include "imgui.h"
+//#include "backends/imgui_impl_opengl3.h"
 
 ModuleEditor::ModuleEditor(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
