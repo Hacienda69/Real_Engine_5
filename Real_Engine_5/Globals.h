e867a0eeeef52c628a3bde306f8095234dc49450
Marc Escandell <marc.escandell.alonso@estudiantat.upc.edu>
2023-10-04 16:58:07 +0200

    go go go go

--- Real_Engine_5/Globals.h
+++ Real_Engine_5/Globals.h
@@ -17,8 +17,6 @@ void log(const char file[], int line, const char* format, ...);
 #define RADTODEG 57.295779513082320876f
 #define PI 3.14159265358979323846264338327950288
 
-//bool AppShouldClose;
-
 typedef unsigned int uint;
 
 enum update_status
